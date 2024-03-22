ba7a70692da7d118e16f7a9d8f751ca4968d001e
rachelschnak <schnakenberg.r@northeastern.edu>
2024-03-22 00:02:59 -0400

    Connected Inventory bars to UserInvetoryObject and added listeners to update values in bars on change to data

--- PolliNation/Assets/Scripts/Hive/HiveInventoryBar.cs
+++ PolliNation/Assets/Scripts/Hive/HiveInventoryBar.cs
@@ -1,22 +1,34 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 
 public class HiveInventoryBar : MonoBehaviour
 {
+    public InventoryScriptableObject UserInventory;
     public TextMeshProUGUI HoneyAmountText;
     public TextMeshProUGUI PropolisAmountText;
     public TextMeshProUGUI RoyalJellyAmountText;
     // Start is called before the first frame update
     void Start()
     {
-        
+        if (UserInventory != null) {
+            HoneyAmountText.text = UserInventory.GetHoneyCount.ToString();
+            PropolisAmountText.text = UserInventory.GetPropolisCount.ToString();
+            RoyalJellyAmountText.text = UserInventory.GetRoyalJellyCount.ToString();
+        }
+        // add listener 
+        UserInventory.OnInventoryChanged += InventoryUpdated;
+    }
+
+    private void InventoryUpdated(object sender, System.EventArgs e) {
+        if (UserInventory != null) {
+            HoneyAmountText.text = UserInventory.GetHoneyCount.ToString();
+            PropolisAmountText.text = UserInventory.GetPropolisCount.ToString();
+            RoyalJellyAmountText.text = UserInventory.GetRoyalJellyCount.ToString();
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
     }
 }
--- PolliNation/Assets/Scripts/Overworld/OverworldInventoryBar.cs
+++ PolliNation/Assets/Scripts/Overworld/OverworldInventoryBar.cs
@@ -1,23 +1,39 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 
 public class OverworldInventoryBar : MonoBehaviour
 {
+    public InventoryScriptableObject UserInventory;
     public TextMeshProUGUI PollenAmountText;
     public TextMeshProUGUI NectarAmountText;
     public TextMeshProUGUI WaterAmountText;
     public TextMeshProUGUI BudsAmountText;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+         if (UserInventory != null) {
+            PollenAmountText.text = UserInventory.GetPollenCount.ToString();
+            NectarAmountText.text = UserInventory.GetNectarCount.ToString();
+            WaterAmountText.text = UserInventory.GetWaterCount.ToString();
+            BudsAmountText.text = UserInventory.GetBudsCount.ToString();
+        }
+        //add listener 
+        UserInventory.OnInventoryChanged += InventoryUpdated;
+    }
+
+    private void InventoryUpdated(object sender, System.EventArgs e) {
+         if (UserInventory != null) {
+            PollenAmountText.text = UserInventory.GetPollenCount.ToString();
+            NectarAmountText.text = UserInventory.GetNectarCount.ToString();
+            WaterAmountText.text = UserInventory.GetWaterCount.ToString();
+            BudsAmountText.text = UserInventory.GetBudsCount.ToString();
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+    
     }
 }
