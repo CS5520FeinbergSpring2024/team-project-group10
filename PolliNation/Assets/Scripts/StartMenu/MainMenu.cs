7cd117245db9317618a7ac67afea1d8436bb420b
zlm23github <122407110+zlm23github@users.noreply.github.com>
2024-03-23 15:56:03 -0400

    Fixed typo, resize screen, remove quit button, use string name to load scene

--- PolliNation/Assets/Scripts/StartMenu/MainMenu.cs
+++ PolliNation/Assets/Scripts/StartMenu/MainMenu.cs
@@ -5,13 +5,10 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
-    public void PlyayGame() 
+    public void PlayGame() 
     {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        string hiveScene = "Hive";
+        SceneManager.LoadScene(hiveScene);
     }
 
-    public void QuitGame() 
-    {
-       Application.Quit();
-    }
 }
