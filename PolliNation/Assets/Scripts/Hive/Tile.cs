0fb62a16bbc5578d5e60aebd60a70bcb694cff3a
zlm23github <122407110+zlm23github@users.noreply.github.com>
2024-03-24 21:17:35 -0400

    Add Tile prefab

--- PolliNation/Assets/Scripts/Hive/Tile.cs
+++ PolliNation/Assets/Scripts/Hive/Tile.cs
@@ -2,17 +2,34 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// I used setActive method to make build menu show or not,
+/// which is faster but cost more memory.
+/// If use setActive method to hide buildMenu at start, should we hide it in anywhere else ? 
+/// </summary>
 public class Tile : MonoBehaviour
 {
+    public GameObject buildMenu;
+    public GameObject self;
     // Start is called before the first frame update
     void Start()
-    {
-        
+    {   
+       //hide the buildMenu at first
+       buildMenu.SetActive(false);
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public void OpenBuildMenu() 
+    {
+        if (buildMenu != null) 
+        {   
+            //show 
+            buildMenu.SetActive (true);
+        }
     }
 }
