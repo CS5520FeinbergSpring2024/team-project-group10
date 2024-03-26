64aeb793b9fb64669cdaa25856c54502661461db
rachelschnak <schnakenberg.r@northeastern.edu>
2024-03-26 18:26:54 -0400

    Revert "got rid of unused method in InventoryMenuScript"
    This reverts commit 0cb7bb44165564d3dac55dcb71ca20bfa857ec3e.

--- PolliNation/Assets/Scripts/Shared/InventoryMenuScript.cs
+++ PolliNation/Assets/Scripts/Shared/InventoryMenuScript.cs
@@ -95,4 +95,14 @@ public class InventoryMenuScript : MonoBehaviour
             Debug.LogWarning("Inventory Scriptable Object asset is null");
         }
     */
+
+    
+    /*
+    public void ExitMenu()
+    {
+        Canvas canvas = GetComponentInParent<Canvas>();
+        canvas.gameObject.SetActive(false);
+        Debug.Log("Inventory Menu Exit button was clicked");
+    }
+    */
 }
