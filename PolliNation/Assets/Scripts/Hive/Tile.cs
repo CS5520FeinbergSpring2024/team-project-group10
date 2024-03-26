d96a8a76ef09c2141e18fe7123c14a468163473d
zlm23github <122407110+zlm23github@users.noreply.github.com>
2024-03-26 14:26:18 -0400

    Fixed the bug that cannot reopen the buildMenu

--- PolliNation/Assets/Scripts/Hive/Tile.cs
+++ PolliNation/Assets/Scripts/Hive/Tile.cs
@@ -3,19 +3,18 @@ using System.Collections.Generic;
 using UnityEngine;
 
 /// <summary>
-/// I used setActive method to make build menu show or not,
+/// Used setActive method to make build menu show or not,
 /// which is faster but cost more memory.
-/// If use setActive method to hide buildMenu at start, should we hide it in anywhere else ? 
 /// </summary>
 public class Tile : MonoBehaviour
 {
-    public GameObject buildMenu;
+    public GameObject buildMenuCanvas;
     public GameObject self;
     // Start is called before the first frame update
     void Start()
     {   
        //hide the buildMenu at first
-       buildMenu.SetActive(false);
+       buildMenuCanvas.SetActive(false);
     }
 
     // Update is called once per frame
@@ -26,10 +25,10 @@ public class Tile : MonoBehaviour
 
     public void OpenBuildMenu() 
     {
-        if (buildMenu != null) 
-        {   
+        if (buildMenuCanvas != null) 
+        {
             //show 
-            buildMenu.SetActive (true);
+            buildMenuCanvas.SetActive(true);
         }
     }
 }
