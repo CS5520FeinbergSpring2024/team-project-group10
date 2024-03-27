d21bea2955f8bc00f5739358da270bb453a54913
felix <fperez2020@outlook.com>
2024-03-26 21:44:45 -0400

    Renamed collision script

--- PolliNation/Assets/Scripts/Overworld/Bee/BeeCollision.cs
+++ PolliNation/Assets/Scripts/Overworld/Bee/BeeCollision.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class BeeCollission : MonoBehaviour
+public class BeeCollision : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
