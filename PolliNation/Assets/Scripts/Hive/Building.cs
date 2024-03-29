4caf7cdc0a5d99611ca7b988233d4ce11d3fdef2
Leigh-Riane Amsterdam <amsterdam.l@northeastern.edu>
2024-03-28 20:54:14 -0400

    Updating building class

--- PolliNation/Assets/Scripts/Hive/Building.cs
+++ PolliNation/Assets/Scripts/Hive/Building.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class Building : MonoBehaviour
 {
+    public InventoryScriptableObject myInventory;
     public BuildingType Type
     { get; set; }
     public int Cost
@@ -27,8 +28,33 @@ public class Building : MonoBehaviour
         Position = position;
     }
 
-    public bool CanAfford()
+    public bool CanAfford(BuildingType type)
     {
+        if (type == BuildingType.Gathering)
+        {
+            if(myInventory.GetResourceCount(global::ResourceType.Nectar) >= 5)
+            {
+                return true;
+            }
+        }
+
+        if(type == BuildingType.Storage)
+        {
+            if((myInventory.GetResourceCount(global::ResourceType.Nectar) >= 10) &&
+                (myInventory.GetResourceCount(global::ResourceType.Pollen) >= 5))
+            {
+                return true;
+            }
+        }
+
+        if(type == BuildingType.Production)
+        {
+            if((myInventory.GetResourceCount(global::ResourceType.Nectar) >= 20) &&
+                    (myInventory.GetResourceCount(global::ResourceType.Pollen) >= 10))
+            {
+                return true;
+            }
+        }
         return true;
     }
 
