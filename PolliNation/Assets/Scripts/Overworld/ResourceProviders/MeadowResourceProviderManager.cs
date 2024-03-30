07cae7591774c8919f00d9d19db7a67bac81fa57
m-siegel <mea.siegel@gmail.com>
2024-03-29 19:50:36 -0700

    Add rotation to random instantiation.

--- PolliNation/Assets/Scripts/Overworld/ResourceProviders/MeadowResourceProviderManager.cs
+++ PolliNation/Assets/Scripts/Overworld/ResourceProviders/MeadowResourceProviderManager.cs
@@ -5,14 +5,22 @@ using UnityEngine;
 
 public class MeadowResourceProviderManager : MonoBehaviour
 {
+    // Fields to in editor.
+
     [SerializeField]
     private int _numPollenProviders = 24;
     [SerializeField]
     private GameObject _pollenProviderPrefab;
+
+    // Other fields.
+
     // For polar locations. Generated locations fall in
     // the ring between these two radii.
     private readonly int _locationOuterRadius = 50;
     private readonly int _locationInnerRadius = 15;
+    // For flower rotations.
+    private readonly int _xRotationDegrees = 10;
+    private readonly int _zRotationDegrees = 10;
 
     /// <summary>
     /// Generates a random Vector3 location on the plane within the 
@@ -31,6 +39,20 @@ public class MeadowResourceProviderManager : MonoBehaviour
     }
 
     /// <summary>
+    /// Generates a Quaternion rotation with random x and z rotations within
+    /// +- _xRotationDegrees and +- _zRotationDegrees, respectively, and a y-rotation
+    /// between 0 and 360.
+    /// </summary>
+    /// <returns></returns>
+    private Quaternion GenerateRandomRotation()
+    {
+        float x = UnityEngine.Random.Range(-_xRotationDegrees, _xRotationDegrees);
+        float z = UnityEngine.Random.Range(-_zRotationDegrees, _zRotationDegrees);
+        float y = UnityEngine.Random.Range(0, 360);
+        return Quaternion.Euler(x, y, z);
+    }
+
+    /// <summary>
     /// Spawn <c>count</c> number of the given <c>prefab</c> at random locations.
     /// </summary>
     /// <param name="prefab">The prefab to spawn objects from.</param>
@@ -43,7 +65,7 @@ public class MeadowResourceProviderManager : MonoBehaviour
             {
                 Instantiate(_pollenProviderPrefab,
                             GenerateRandomRingLocation(),
-                            Quaternion.identity);
+                            GenerateRandomRotation());
             }
 
         }
