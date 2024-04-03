b57b8edba3b964005019256d03b52000db9f1706
m-siegel <mea.siegel@gmail.com>
2024-04-03 11:33:12 -0700

    Remove left-over comment

--- PolliNation/Assets/Scripts/Overworld/ResourceProviders/NectarProvider.cs
+++ PolliNation/Assets/Scripts/Overworld/ResourceProviders/NectarProvider.cs
@@ -5,7 +5,6 @@ public class NectarProvider : FlowerResourceProvider
 {
     new void Awake() {
         base.Awake();
-        // Not really necessary since it defaults to pollen.
         SetValues(ResourceType.Nectar);
         TotalRegenerationCycles = 3;
     }
