af7316cc0ccb85cc9ff7e084944199fde7453b82
felix <fperez2020@outlook.com>
2024-04-04 00:43:44 -0400

    RewardType enum

---
+++ PolliNation/Assets/Scripts/Shared/RewardType.cs
@@ -0,0 +1,16 @@
+/**
+ <summary>
+ All reward types, including raw/produced resources and workers.
+ </summary>
+ */
+public enum RewardType
+{
+    Pollen,
+    Nectar,
+    Buds,
+    Water,
+    Honey,
+    Propolis,
+    RoyalJelly,
+    Workers
+}
