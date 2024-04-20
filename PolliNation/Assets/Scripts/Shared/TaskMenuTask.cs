9905c553e01d377f735ddda265a3b869c3309aaf
rachelschnak <schnakenberg.r@northeastern.edu>
2024-04-19 23:06:58 -0400

    got rid on unused imports

--- PolliNation/Assets/Scripts/Shared/TaskMenuTask.cs
+++ PolliNation/Assets/Scripts/Shared/TaskMenuTask.cs
@@ -1,9 +1,7 @@
 using System;
 using System.Collections.Generic;
 using TMPro;
-using UnityEditor.SearchService;
 using UnityEngine;
-using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class TaskMenuTask : MonoBehaviour
